24dd0d508863dc918467d292f2120fb9795bc848
Gabriel Russo <gabrielrusso@protonmail.com>
2023-01-19 17:16:12 -0400

    Exercícios de ponteiros

--- Cpp/Ponteiros/Ex1/main.cpp
+++ Cpp/Ponteiros/Ex1/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 
-int main(){
+// Write a program that asks the user to enter integers as inputs to be stored 
+// in the variables 'a' and 'b' respectively. There are also two integer pointers named 
+// ptrA and ptrB. Assign the values of 'a' and 'b' to ptrA and ptrB respectively, and display them.
 
-	std::cout << "hello world" << std::endl;
+int main(){    
+    
+    int a;
+    int b;
+
+    std::cout << "Variable A: ";
+    std::cin >> a;
+
+    std::cout << "Variable B: ";
+    std::cin >> b;
+
+    int* ptrA = &a;
+    int* ptrB = &b;
+
+    std::cout << "Value of variable A: " << *ptrA << " Address of variable A: " << ptrA << std::endl;
+
+    std::cout << "Value of variable B: " << *ptrB << " Address of variable B: " << ptrB << std::endl;
 
 	return 0;
 }
---
+++ Cpp/Ponteiros/Ex2/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+// Write a C++ program to find the max of an integral data set. The program will ask 
+// the user to input the number of data values in the set and each value. 
+// The program prints on screen a pointer that points to the max value.
+
+int main(){
+
+    int array_lenght;
+
+    std::cout << "How many numbers in data set?: ";
+    std::cin >> array_lenght;
+
+    int array[array_lenght];
+
+    int i = 0;
+    while (i <= array_lenght){
+        int value;        
+
+        std::cout << "Enter the value " << i << " of " << array_lenght << " :";
+        std::cin >> value;
+
+        array[i] = value;
+
+        i++; 
+    }
+    
+    int* ptr = nullptr;
+
+    for (i = 0; i <= array_lenght; i++){
+        if (!ptr)
+            ptr = &array[i];
+
+        if (*ptr < array[i])
+            ptr = &array[i];
+    }
+
+	std::cout << "O maior valor do array é: "<< *ptr << std::endl;
+
+	return 0;
+}
+
